8fbb006c6d1b71c928adacf168b35b665ed7812d
Daniel Yu <daniel.sm.yu@gmail.com>
2019-02-28 21:44:21 -0500

    initial backend commit

--- app/src/main/java/com/dsyu/jacques/MainActivity.java
+++ app/src/main/java/com/dsyu/jacques/MainActivity.java
@@ -1,13 +1,118 @@
 package com.dsyu.jacques;
 
+import android.nfc.Tag;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import static com.dsyu.jacques.R.drawable.black;
+import static com.dsyu.jacques.R.drawable.card1;
 
 public class MainActivity extends AppCompatActivity {
+                                            public static final String TAG = MainActivity.class.toString();
+    private int[] cardValues = new int[8];
+    private boolean[] playerHand = new boolean[4];
+    private boolean[] cpuHand = new boolean[4];
+    private ImageButton card1, card2, card3, card4, card5, card6, card7, card8;
+    private Button jacquesButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        card1 = findViewById(R.id.card1);
+        card2 = findViewById(R.id.card2);
+        card3 = findViewById(R.id.card3);
+        card4 = findViewById(R.id.card4);
+        card5 = findViewById(R.id.card5);
+        card6 = findViewById(R.id.card6);
+        card7 = findViewById(R.id.card7);
+        card8 = findViewById(R.id.card8);
+        jacquesButton = findViewById(R.id.jacquesButton);
+
+        // Randomly generates unique values for the 8 cards
+        ArrayList<Integer> deck = new ArrayList<Integer>();
+        for (int i = 1; i < 14; i++) {
+            deck.add(i);
+        }
+        for (int i = 0; i < 8; i++) {
+            int num = (int) (Math.random () * (13-i));
+            cardValues[i] = deck.get(num);
+            deck.remove(num);
+        }
+
+        // Removes every player card not in player's hand
+        jacquesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!playerHand[0]) {
+                    card1.setImageResource(R.drawable.black);
+                    playerHand[0] = true;
+                }
+                if (!playerHand[1]) {
+                    card2.setImageResource(R.drawable.black);
+                    playerHand[1] = true;
+                }
+                if (!playerHand[2]) {
+                    card3.setImageResource(R.drawable.black);
+                    playerHand[2] = true;
+                }
+                if (!playerHand[3]) {
+                    card4.setImageResource(R.drawable.black);
+                    playerHand[3] = true;
+                }
+            }
+        });
+
+        // Updates player cards with clicked
+        card1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!playerHand[0]) {
+                    card1.setImageResource(R.drawable.card6);
+                    playerHand[0] = true;
+                }
+            }
+        });
+
+        card2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!playerHand[1]) {
+                    card2.setImageResource(R.drawable.card11);
+                    playerHand[1] = true;
+                }
+            }
+        });
+
+        card3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!playerHand[2]) {
+                    card3.setImageResource(R.drawable.card1);
+                    playerHand[2] = true;
+                }
+            }
+        });
+
+        card4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!playerHand[3]) {
+                    card4.setImageResource(R.drawable.card13);
+                    playerHand[3] = true;
+                }
+            }
+        });
+
+
+
     }
 }
